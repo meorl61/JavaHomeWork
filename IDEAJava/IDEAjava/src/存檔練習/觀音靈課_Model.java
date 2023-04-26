7a3312f64a8f83f16b95bbb6481f18232036c2b9
meorl61 <60282527+meorl61@users.noreply.github.com>
2023-04-26 13:51:10 +0800

    觀音靈課抽籤

---
+++ IDEAJava/IDEAjava/src/存檔練習/觀音靈課_Model.java
@@ -0,0 +1,21 @@
+package 存檔練習;
+
+import java.io.Serializable;
+
+public class 觀音靈課_Model implements Serializable {
+    int 籤號;
+    String 卦名;
+    String 吉凶;
+    String 卦爻;
+    String 卦詞;
+    String 卦義;
+
+    public void set資料(int 籤號,String 卦名,String 吉凶,String 卦爻,String 卦詞,String 卦義) {
+        this.籤號 = 籤號;
+        this.卦名 = 卦名;
+        this.吉凶 = 吉凶;
+        this.卦爻 = 卦爻;
+        this.卦詞 = 卦詞;
+        this.卦義 = 卦義;
+    }
+}
---
+++ IDEAJava/IDEAjava/src/存檔練習/讀存檔.java
@@ -0,0 +1,39 @@
+package 存檔練習;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+public class 讀存檔 {
+    private 觀音靈課Data 資料;
+    private final String filename="src/存檔練習/觀音靈課32籤1";
+
+    public void set資料(觀音靈課Data 資料) {
+        this.資料 = 資料;
+    }
+
+    public 觀音靈課Data get資料() {
+        return 資料;
+    }
+
+    public void 存檔() throws Exception{
+        FileOutputStream 開檔 = new FileOutputStream(filename);
+        ObjectOutputStream 輸出物件 = new ObjectOutputStream(開檔);
+        輸出物件.writeObject(this.資料);
+        輸出物件.close();
+        開檔.close();
+
+    }
+
+    public void 讀檔() throws Exception{
+
+        FileInputStream 開檔 = new FileInputStream(filename);
+        ObjectInputStream 讀入物件 = new ObjectInputStream(開檔);
+        this.資料 = (觀音靈課Data) 讀入物件.readObject();
+        讀入物件.close();
+        開檔.close();
+
+    }
+}
